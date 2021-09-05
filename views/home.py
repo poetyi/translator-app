08c0504a172ae0f088e810d391f086d247213d8c
poetyi <wirthervin@gmail.com>
2021-09-05 22:22:32 +0200

    json output, add dict results

--- views/home.py
+++ views/home.py
@@ -9,13 +9,14 @@ def search():
     language_from: str = request.args.get('from', default=None, type=str)
     language_to: str = request.args.get('to', default=None, type=str)
     form: str = request.args.get('form', default=None, type=str)
+    format: str = request.args.get('format', default=None, type=str)
 
     print(language_from, language_to, form)
 
     if (language_from and form):
               
         ordered_dict = json.load(open('static/data/ordered.json', encoding='utf-8')) 
-        print(ordered_dict)
+
         try:
             entity_ids = ordered_dict[language_from][form]
         except:
@@ -25,16 +26,19 @@ def search():
             entities_dict = json.load(open('static/data/entities.json', encoding='utf-8'))
 
             if isinstance(entity_ids, list): # There are homonymes
-                results = []
-                for entity_id in entity_ids:
-                    results.append(entities_dict[str(entity_id)])
+                results = {}
+                for i in range(len(entity_ids)):
+                    results[i] = entities_dict[str(entity_ids[i])]
             
             else: # Only one match
-                results = [entities_dict[str(entity_ids)]]
+                results = entities_dict[str(entity_ids)]
         
         else:
-            results = ["No match."]
+            results = "No match."
     else:
-        results = []
+        results = None
 
-    return render_template('home/index.html', results = results)
+    if format == 'json':
+        return results, 200 # Still need some output edit (no lists, only dictionary with results)
+
+    return render_template('home/index.html', language_from = language_from, language_to = language_to, form = form, results = results)
